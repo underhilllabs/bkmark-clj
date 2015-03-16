14e97a5b46952f8da7884b7d3bae044625f7b1bf
Bart Lantz <underhilllabs@gmail.com>
2015-03-16 08:07:13 -0600

    Adds some view functions

--- src/views/bookmarks.clj
+++ src/views/bookmarks.clj
@@ -1,4 +1,13 @@
 (ns bkmarker.views.bookmarks
   (:require [hiccup/core :refer :all]))
 
+(defn layout [content]
+  (html
+    [:head [:title "Bkmarker| Social Bookmarking"]]
+    [:body content]))
 
+(defn view-bookmark [bkmark]
+  (html
+   [:a {:href (get bkmark :url)} {:title bkmark}]
+   [:br]
+   [:p [:span (str {:updated_at bkmark} " by " {:users.username bkmark})]]))
