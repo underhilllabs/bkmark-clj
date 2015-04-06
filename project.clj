d9048f99950223cdbcba9d0353c801682936ce52
Bart Lantz <underhilllabs@gmail.com>
2015-04-05 21:22:26 -0600

    adds enlive to project

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [org.clojure/java.jdbc "0.3.6"]
                  [mysql/mysql-connector-java "5.1.25"]
                  [korma "0.4.0"]
+                 [enlive "1.1.5"]
                  [compojure "1.1.8"]
                  [com.cemerick/friend "0.2.1"]
                  [ragtime "0.3.8"]
@@ -14,8 +15,7 @@
                  [digest "1.4.4"] ;; for gravatar md5
                  [hiccup "1.0.5"]]
   :plugins [[ragtime/ragtime.lein "0.3.8"]]
-    :ragtime
-    {:migrations ragtime.sql.files/migrations
-    :database "jdbc:mysql://localhost:3306/bkmarker_dev?user=db_user_name_here&password=db_user_password_here"}
+  :ragtime {:migrations ragtime.sql.files/migrations
+            :database "jdbc:mysql://localhost:3306/bkmarker_dev?user=db_user_name_here&password=db_user_password_here"}
 
   :main bkmarker.core)
