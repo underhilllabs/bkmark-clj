e19e3b7ffa7b749f841aa5ab7ce3fbbd1545f6e6
Bart Lantz <underhilllabs@gmail.com>
2016-03-25 11:16:25 -0400

    updates dbconn

--- src/bkmarker/db/dbconn.clj
+++ src/bkmarker/db/dbconn.clj
@@ -2,9 +2,17 @@
   (:require [korma.core :refer :all]
             [korma.db :refer :all]))  
 
-(defdb prod (mysql {:db "bkmarker"
+(defdb prod (mysql {:db "bkmarker_prod"
                     :port 3306
                     :host "localhost"
                     :delimiters "`"
-                    :user "bkmarker"
-                    :password "change-THIS-password"}))
+                    :user "root"
+                    :password "23skidoo"}))
+
+(def db-spec 
+  {:classname "com.mysql.jdbc.Driver"
+   :subprotocol "mysql"
+   :subname "//127.0.0.1:3306/bkmarker_prod"
+   :user "root"
+   :password "23skidoo"})
+
